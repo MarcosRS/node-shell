4992979a8cc40e4cf523225604fd3d9d7fbeaced
MarcosRS <MarcosRS16@gmail.com>
2017-03-06 15:10:30 -0500

    cat-head-tail

--- command.js
+++ command.js
@@ -12,12 +12,46 @@ module.exports = {
         fs.readdir('.', function(err, files) {
             if (err) throw err;
             files.forEach(function(file) {
-                console.log( file.toString());
+                console.log(file.toString());
             })
         });
         return ""
     },
-    echo:function(str){
-    	return str;
+    echo: function(str) {
+        return str;
+    },
+    cat: function(filename) {
+        fs.readFile(filename, "UTF8", function(err, data) {
+            if (err) {
+                throw err
+            };
+            var fileContent = data;
+            console.log(fileContent);
+        });
+        return ""
+    },
+    head: function(filename) {
+    	 
+        fs.readFile(filename, "UTF8", function(err, data) {
+            if (err) {
+                throw err
+            };
+            var fileContent = data;
+            fileContent.split('\n');
+            console.log(fileContent.split('\n').slice(0,5).join("\n"));
+        });
+        return ""
+    },
+    tail: function(filename) {
+    	
+        fs.readFile(filename, "UTF8", function(err, data) {
+            if (err) {
+                throw err
+            };
+            var fileContent = data;
+            var difference = fileContent.split('\n').length - 5;
+            console.log(fileContent.split('\n').slice(difference ,fileContent.length).join("\n"));
+        });
+        return ""
     }
 }
